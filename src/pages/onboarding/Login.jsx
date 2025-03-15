2a535190acc89b1fdb5bd6421133fd77fc7a5935
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-03-15 03:14:35 +0100

    Working on onboarding

--- src/pages/onboarding/Login.jsx
+++ src/pages/onboarding/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Mail, Lock, UserCircle } from "lucide-react";
-// import googleLogo from "../../assets/img/googleLogo.png";
+import googleLogo from "../../assets/google.svg";
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
@@ -23,27 +23,16 @@ const Login = () => {
         </div>
 
         <div className="w-full max-w-md p-4 rounded-t-xl shadow-md">
-          <p className=" text-white">Login with</p>
-
-          <button className="w-full flex items-center justify-center border border-gray-300 rounded-md py-2 mt-2 text-white cursor-pointer">
-            {/* <img src={googleLogo} alt="Google" className="w-5 h-5 mr-2" /> */}
-            Google
-          </button>
-
-          <div className="flex items-center my-4">
-            <hr className="flex-grow border-gray-300" />
-            <span className="px-2 text-white text-sm">or login in with</span>
-            <hr className="flex-grow border-gray-300" />
-          </div>
           <form onSubmit={handleSubmit} className="space-y-4">
             {/* Email Field */}
-            <div className="flex items-center p-2 bg-gray-200 rounded-lg">
-              <Mail className="text-gray-500 ml-2" />
+            <div className="flex p-2 px-4 bg-gray-200 rounded-lg flex-col shadow-md">
+              <label htmlFor="email" className="font-semibold">
+                Email
+              </label>
               <input
                 type="email"
                 name="email"
-                placeholder="Enter your email"
-                className="w-full p-2 bg-transparent border-none outline-none focus:outline-none focus:ring-0 text-black"
+                className="w-full bg-transparent border-none outline-none focus:outline-none focus:ring-0 text-black"
                 value={formData.email}
                 onChange={handleChange}
                 required
@@ -51,23 +40,38 @@ const Login = () => {
             </div>
 
             {/* Password Field */}
-            <div className="flex items-center p-2 bg-gray-200 rounded-lg">
-              <Lock className="text-gray-500 ml-2" />
+            <div className="flex p-2 px-4 bg-gray-200 rounded-lg flex-col shadow-md">
+              <label htmlFor="password" className="font-semibold">
+                Password
+              </label>
               <input
                 type="password"
                 name="password"
-                placeholder="Enter your password"
-                className="w-full p-2 bg-transparent border-none outline-none focus:outline-none focus:ring-0 text-black"
+                className="w-full bg-transparent border-none outline-none focus:outline-none focus:ring-0 text-black"
                 value={formData.password}
                 onChange={handleChange}
                 required
               />
             </div>
 
+            <div className="flex items-center my-4">
+              <hr className="flex-grow border-gray-300" />
+              <span className="px-2 text-white text-sm">or login in with</span>
+              <hr className="flex-grow border-gray-300" />
+            </div>
+
+            <button
+              className="w-full flex items-center justify-center border border-gray-300 rounded-md py-2 mt-4 text-white cursor-pointer text-sm hover:scale-105 transition-transform"
+              type="button"
+            >
+              <img src={googleLogo} alt="Google" className="w-5 h-5 mr-2" />
+              Google
+            </button>
+
             {/* Submit Button */}
             <button
               type="submit"
-              className="w-full bg-[#fb8500] text-white py-3 rounded-full  transition"
+              className="w-full bg-[#fb8500] text-white py-3 rounded-2xl rounded-tr-none  transition font-sans"
             >
               Next
             </button>
